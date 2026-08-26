6c883c928b009ed4affa3c9e4a6a474541036fd8
Nontobeko <nontoncube7@gmail.com>
2026-08-26 10:13:29 +0200

    create basic Flask app, and user table in cleverspender.db

---
+++ Backend/db.py
@@ -0,0 +1,18 @@
+#sqlite3 is built into the standard library, so we don't need to install it separately
+import sqlite3
+def connect_db():
+    conn = sqlite3.connect('cleverspender.db')
+    return conn
+
+def create_tables():
+    conn= connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE
+        )
+''')
+    conn.commit()
+    conn.close()
