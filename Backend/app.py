31d8abf707f26b830bf64ce0c749317990689dc1
Nontobeko <nontoncube7@gmail.com>
2026-08-31 13:57:33 +0200

    update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-from db import create_tables
+from flask import Flask, request, jsonify
+from db import create_tables, connect_db
+from user import User
 
 app = Flask(__name__)
 create_tables()
 
 @app.route('/')
 def home():
-    return {"message": "CleverSpender API is running!"}
+    return jsonify({"message": "CleverSpender API is running! 🚀"})
+
+@app.route('/user/register', methods=['POST'])
+def register_user():
+    data = request.get_json()  # gets JSON sent from frontend
+    name = data['name']
+    email = data['email']
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    # saves new user to database
+    cursor.execute('INSERT INTO users (name, email) VALUES (?, ?)', (name, email))
+    conn.commit()
+    user_id = cursor.lastrowid  # gets the id SQLite just assigned
+    conn.close()
+
+    user = User(name, email, user_id)
+    return jsonify(user.get_profile()), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
