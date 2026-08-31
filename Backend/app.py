daf82b854e0841038e23242ca37db7ca76efa17d
Nontobeko <nontoncube7@gmail.com>
2026-08-31 13:31:10 +0200

    create tables reminder, nudges_log and schedules in database

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from db import create_tables
+
 app = Flask(__name__)
+create_tables()
+
 @app.route('/')
 def home():
     return {"message": "CleverSpender API is running!"}
--- Backend/db.py
+++ Backend/db.py
@@ -1,9 +1,12 @@
 #sqlite3 is built into the standard library, so we don't need to install it separately
 import sqlite3
+
+#get connection to the database
 def connect_db():
     conn = sqlite3.connect('cleverspender.db')
     return conn
 
+# create the tables in the database
 def create_tables():
     conn= connect_db()
     cursor = conn.cursor()
@@ -14,6 +17,42 @@ def create_tables():
         email TEXT NOT NULL UNIQUE
         )
 ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS reminders (
+        reminder_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        focus TEXT NOT NULL,
+        goal TEXT NOT NULL,
+        is_active INTEGER,
+        paused_until TEXT,
+        created_at TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS schedules (
+        schedule_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        reminder_id INTEGER NOT NULL,
+        time_window TEXT NOT NULL,
+        days TEXT NOT NULL,
+        exact_start TEXT,
+        exact_end TEXT,
+        FOREIGN KEY (reminder_id) REFERENCES reminders(reminder_id)
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nudge_logs (
+        log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        response TEXT NOT NULL,
+        reminder_id INTEGER NOT NULL,
+        sent_at TEXT NOT NULL,
+        responded_at TEXT,
+        FOREIGN KEY (reminder_id) REFERENCES reminders(reminder_id)
+        )
+    ''')
+
     conn.commit()
     conn.close()
-    
