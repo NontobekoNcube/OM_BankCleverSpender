321da9193762d768c1d7bc346af0b8eac0dded6a
Nontobeko <nontoncube7@gmail.com>
2026-09-01 03:07:02 +0200

    update user class- created get_profile method

--- Backend/user.py
+++ Backend/user.py
@@ -2,14 +2,14 @@
 class User:
     def __init__(self, name, email, user_id=None):
         #user is None for new users, -assigned by SQLite on save.
-        self.id = user_id
+        self.user_id = user_id
         self.name = name
         self.email = email
 
     def get_profile(self):
         # returns user data as a dictionary fofr the API to send back.
         return {
-            "id": self.id,
+            "user_id": self.id,
             "name": self.name,
             "email": self.email
         }
