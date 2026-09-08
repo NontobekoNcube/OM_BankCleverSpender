7453cbd358beff720e1452ced142eb83ca879d7f
Nontobeko <nontoncube7@gmail.com>
2026-09-08 12:07:24 +0200

    input data validation

--- Backend/app.py
+++ Backend/app.py
@@ -20,13 +20,30 @@ def home():
 
 @app.route('/user/register', methods=['POST'])
 def register_user():
-    data = request.get_json()  # gets JSON sent from frontend
-    name = data['name']
-    email = data['email']
+    # get the JSON from the frontend and convert it to a Python dictionary
+    data = request.get_json()
 
+    # read name and email from the dictionary   
+    name = data.get('name')
+    email = data.get('email')
+
+    #data validation: check if name and email are provided in the right format,
+    #used individual checks to be specific about what is wrong with the input, instead of a generic error message for user comfort and clarity
+    if not name:
+        return jsonify({"error": "Invalid input. Name is required."}), 400
+    if not email:
+        return jsonify({"error": "Invalid input. Email is required."}), 400
+    if '@' not in email or '.' not in email:
+        return jsonify({"error": "Invalid input. Email format is incorrect."}), 400
+
+    # create a new User object with the values from the dictionary
     new_user = User(name=name, email=email)
+
+    # add to the session(staging area) then commit(save to database)
     db.session.add(new_user)
     db.session.commit()
+
+    # return the new user's profile as JSON with 201 Created status
     return jsonify(new_user.get_profile()), 201
 
 @app.route('/user/<int:user_id>', methods=['GET'])
